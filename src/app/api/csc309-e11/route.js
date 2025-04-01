d336c57562c98056f644b7f923025d7ff77389bb
mary <mary4net@gmail.com>
2025-04-01 12:10:20 -0400

    next

--- src/app/api/csc309-e11/route.js
+++ src/app/api/csc309-e11/route.js
@@ -1,10 +1,9 @@
-export default function handler(req, res) {
-  res.setHeader("Content-Type", "text/html");
-  res.status(200).send(`
-    <div id="auto-tester">
+import { NextResponse } from "next/server";
+export async function GET() {
+  return new NextResponse(
+    `<div id="auto-tester">
       Hi. I am xuruili, and I have learned how to develop a full-stack web app
       and deploy it!
-    </div>
-  `);
+    </div>`,
+  );
 }
-
