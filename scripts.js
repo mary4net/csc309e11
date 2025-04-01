a13040f9c584c9f15cf9bcda733a9fb21eb3d391
mary <mary4net@gmail.com>
2025-04-01 12:02:55 -0400

    page

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 function getBaseUrl() {
 	// return the base URL of your deployed app (only the protocol + domain)
+	return "https://csc309e11-h4aq02gjp-ruilis-projects-0c950137.vercel.app"
 }
 
 function getUTorId() {
