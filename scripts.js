f9c6c7af3ddc169f0a6590f74176e64bc5663571
mary <mary4net@gmail.com>
2025-04-01 11:55:58 -0400

    moved

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function getBaseUrl() {
+	// return the base URL of your deployed app (only the protocol + domain)
+}
+
+function getUTorId() {
+	return "xuruili"
+}
+
+module.exports = {
+	getBaseUrl,
+	getUTorId,
+};
